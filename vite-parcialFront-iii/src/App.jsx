ed84028d7b4dc1779fc0377cef7a0d00bf7f36bb
sebabrun <sebastian.brun44@gmail.com>
2023-08-16 19:02:06 -0300

    inicio repo

---
+++ vite-parcialFront-iii/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./Components/Card.jsx";
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+    <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+      </>
+  );
+}
+
+
+export default App;
+
