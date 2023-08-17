cbb57688469b26b4d191838d99aee6baf11d690c
sebabrun <sebastian.brun44@gmail.com>
2023-08-16 21:04:28 -0300

    subir faltante

--- vite-parcialFront-iii/src/App.jsx
+++ vite-parcialFront-iii/src/App.jsx
@@ -1,17 +1,21 @@
-import Card from "./Components/Card.jsx";
+import React, { useState } from 'react';
+import Card from "./Components/Card/Card.jsx";
+import Form from "./Components/Form/Form.jsx";
 import './App.css'
 
-
 function App() {
+  const [datosUsuario, setUserData] = useState({
+    name: '',
+    auto: ''
+  });
+
   return (
-    <>
-    <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
-      </>
+    <div>
+      <h1>Carga de autos favoritos</h1>
+      <Form submitForm={setUserData} />
+      {datosUsuario.name && datosUsuario.auto && <Card name={datosUsuario.name} auto={datosUsuario.auto} />}
+    </div>
   );
 }
 
-
 export default App;
-
